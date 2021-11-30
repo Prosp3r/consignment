60526d7d8364e8b638bb34e827d1183326a481d1
Prosper <sirpros@gmail.com>
2021-11-30 03:11:44 +0100

    Regenerated proto

--- server/main.go
+++ server/main.go
@@ -51,7 +51,7 @@ func (s *service) CreateConsignment(ctx context.Context, req *pb.Consignment) (*
 
 func (s *service) GetConsignments(ctx context.Context, req *pb.GetRequest) (*pb.Response, error) {
 	consignments := s.repo.GetAll()
-	return &pb.Response{Consignment: consignments}, nil
+	return &pb.Response{Consignments: consignments}, nil
 }
 
 func main() {
