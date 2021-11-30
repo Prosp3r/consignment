7fbd4d74da50b2676febecc58458f69c05f58226
Prosper <sirpros@gmail.com>
2021-11-30 01:27:13 +0100

    CLI Client created

--- cli/main.go
+++ cli/main.go
@@ -1,9 +1,51 @@
-package main 
+package main
 
-import(
-	""
+import (
+	"context"
+	"encoding/json"
+	"io/ioutil"
+	"log"
+	"os"
+
+	pb "github.com/Prosp3r/consignment/proto"
+	"google.golang.org/grpc"
 )
 
-func main(){
+const (
+	address         = "localhost:50051"
+	defaultFilename = "consignment.json"
+)
 
+func parseFile(file string) (*pd.Consignment, error) {
+	var consignment *pb.Consignment
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	json.Unmarshal(data, &consigment)
+	return consignment, err
+}
+
+func main() {
+	conn, err := grpc.Dial(address, grpc.WithInsecure())
+	if err != nil {
+		log.Fatalf("Didi not connect : %v", err)
+	}
+	defer conn.Close()
+	client := pb.NewShippingServiceClient(conn)
+
+	file := defaultFilename
+	if len(os.Args) > 1 {
+		file = os.Args[1]
+	}
+
+	consignment, err := parseFile(file)
+	if err != nil {
+		log.Fatalf("Could not parse file: %v", err)
+	}
+	r, err := client.CreateConsignment(context.Background(), consignment)
+	if err != nil {
+		log.Fatalf("Could not greet: %v", err)
+	}
+	log.Printf("Created: %t", r.Created)
 }
